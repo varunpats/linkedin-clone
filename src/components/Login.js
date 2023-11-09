249696a27ef59beaac50051415c6fc41210e0483
varunpats <varunpatil2502@gmail.com>
2023-11-09 19:52:02 +0530

    Completed login page design

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import logo from '../images/login-logo.svg';
+import hero from '../images/login-hero.svg';
+import google from '../images/google.svg';
 
 const Login = () => {
     return (
@@ -8,7 +10,7 @@ const Login = () => {
                 <a href='/'>
                     <img src={logo} />
                 </a>
-                <div style={{display: "flex" }}>
+                <div style={{ display: "flex" }}>
                     <Join>
                         Join now
                     </Join>
@@ -17,6 +19,18 @@ const Login = () => {
                     </SignIn>
                 </div>
             </Nav>
+            <Section>
+                <Hero>
+                    <h1>Welcome to your professional community</h1>
+                    <img src={hero} />
+                </Hero>
+                <Form>
+                    <Google>
+                        <img src={google} />
+                        Sign in with Google
+                    </Google>
+                </Form>
+            </Section>
         </Container>
     )
 }
@@ -48,6 +62,7 @@ const Join = styled.nav`
     padding: 10px 12px;
     text-decoration: none;
     border-radius: 4px;
+    font-weight: 600;
     color: rgba(0, 0, 0, 0.6);
     margin-right: 12px;
 
@@ -77,5 +92,90 @@ const SignIn = styled.nav`
         text-decoration: none;
     }
 `
+
+const Section = styled.div`
+    display: flex;
+    align-content: start;
+    min-height: 700px;
+    padding-bottom: 138px;
+    padding-top: 40px;
+    padding: 60px 0;
+    position: relative;
+    flex-wrap: wrap;
+    width: 100%;
+    max-width: 1128px;
+    align-items: center;
+    margin: auto;
+
+    @media (max-width: 768px) {
+        margin: auto;
+        min-height: 0px;
+    }
+`
+
+const Hero = styled.div`
+    width: 100%;
+    h1 {
+        padding-bottom: 0;
+        width: 55%;
+        font-size: 56px;
+        color: #2977c9;
+        font-weight: 200;
+        line-height: 70px;
+        @media (max-width: 768px) {
+            text-align: center;
+            font-size: 20px;
+            width: 100%;
+            line-height: 2;
+        }
+    }
+
+    img {
+        width: 700px;
+        height: 670px;
+        position: absolute;
+        bottom: -2px;
+        right: -150px;
+        @media (max-width: 768px) {
+            top: 230px;
+            width: initial;
+            position: initial;
+            height: initial;
+        }
+    }
+`;
+
+const Form = styled.div`
+    margin-top: 100px;
+    width: 408px;
+    @media (max-width: 768px) {
+        margin-top: 20px;
+    }
+`;
+
+const Google = styled.button`
+    display: flex;
+    justify-content: center;
+    background-color: #fff;
+    align-items: center;
+    height: 56px;
+    width: 100%;
+    border-radius: 28px;
+    box-shadow: inset 0 0 0 1px rgb(0 0 0 / 60%),
+        inset 0 0 0 2px rgb(0 0 0 / 0%) inset 0 0 0 1px rgb(0 0 0 / 0);
+
+    vertical-align: middle;
+    z-index: 0;
+    transition-duration: 167ms;
+    font-size: 20px;
+    color: rgba(0, 0, 0, 0.6);
+    &:hover {
+        background-color: rgba(207, 207, 207, 0.25);
+        color: rgba(0, 0, 0, 0.75);
+    }
+    img{
+        margin-right: 10px;
+    }
+`;
 
 export default Login;
