38202f0d5b92097524882fd1ecba5462839fa034
varunpats <varunpatil2502@gmail.com>
2023-11-10 10:26:48 +0530

    Designed Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Login from './components/Login';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
   const appRoutes = createBrowserRouter([
-    { path: '/', element: <Login /> }
+    { path: '/', element: <Login /> },
+    { path: '/home', element: <Home /> }
   ])
-  
+
   return <RouterProvider router={appRoutes} />;
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import Header from "./Header";;
+
+export default function Home() {
+    return (
+        <>
+            <Header />
+            <div>Home</div>
+        </>
+    )
+}
