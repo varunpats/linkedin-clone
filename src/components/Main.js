842467685ab2d3acc3d4a4749b2a8628ce63d64e
varunpats <varunpatil2502@gmail.com>
2023-11-12 16:05:00 +0530

    Designed start post sction

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,33 @@
 import styled from "styled-components";
+import user from '../images/user.svg';
 
 export default function Main() {
     return (
         <Container>
             <ShareBox>
                 Share
+                <div>
+                    <img src={user} />
+                    <button>Start a post</button>
+                </div>
+
+                <div>
+                    <button>
+                        <span>Photo</span>
+                    </button>
+
+                    <button>
+                        <span>Video</span>
+                    </button>
+
+                    <button>
+                        <span>Event</span>
+                    </button>
+
+                    <button>
+                        <span>Write article</span>
+                    </button>
+                </div>
             </ShareBox>
         </Container>
     )
@@ -31,4 +54,52 @@ const ShareBox = styled(CommonCard)`
     color: #958b7b;
     margin: 0 0 8px;
     background: #fff;
+    div {
+        button {
+            outline: none;
+            color: rgba(0, 0, 0, 0.6);
+            font-size: 14px;
+            line-height: 1.5;
+            min-height: 48px;
+            background: transparent;
+            border: none;
+            display: flex;
+            align-items: center;
+            font-weight: 600;
+        }
+        &:first-child {
+            display: flex;
+            align-items: center;
+            padding: 8px 16px 0 16px;
+            img {
+                width: 48px;
+                border-radius: 50%;
+                margin-right: 8px;
+            }
+            button {
+                margin: 4px 0;
+                flex-grow: 1;
+                border-radius: 35px;
+                padding-left: 16px;
+                border: 1px solid rgba(0, 0, 0, 0.15);
+                backgroun-color: #fff;
+                text-align: left; 
+            }
+        }
+        &:nth-child(2) {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-around;
+            padding-bottom: 4px;
+
+            button {
+                img {
+                    margin: 0 4px 0 -2px;
+                }
+                span {
+                    color: #70b5f9;
+                }
+            }
+        }
+    }
 `
