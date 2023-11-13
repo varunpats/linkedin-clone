a5abb8c40b9abcce7de068b8675f38c80fdabe69
varunpats <varunpatil2502@gmail.com>
2023-11-13 21:43:39 +0530

    Created post design and setup for redux

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,14 @@ import user from '../images/user.svg';
 import media from '../images/media-svg.svg';
 import events from '../images/events-svg.svg';
 import article from '../images/article-svg.svg';
+import ellipsis from '../images/ellipsis.svg';
+import image from '../images/shared-image.jpg';
+import like from '../images/like.svg';
+import clapping from '../images/clapping.svg';
+import likes from '../images/likes.svg';
+import comments from '../images/comments.svg';
+import repost from '../images/repost.svg';
+import share from '../images/share.svg';
 
 export default function Main() {
     return (
@@ -30,6 +38,63 @@ export default function Main() {
                     </button>
                 </div>
             </ShareBox>
+            <div>
+                <Article>
+                    <SharedActor>
+                        <a>
+                            <img src={user} />
+                            <div>
+                                <span>Title</span>
+                                <span>Info</span>
+                                <span>Date</span>
+                            </div>
+                        </a>
+                        <button>
+                            <img src={ellipsis} style={{ width: '25px' }} />
+                        </button>
+                    </SharedActor>
+                    <Description>
+                        Description
+                    </Description>
+                    <SharedImg>
+                        <a>
+                            <img src={image} />
+                        </a>
+                    </SharedImg>
+                    <SocialCounts>
+                        <li>
+                            <button>
+                                <img src={like} />
+                                <img src={clapping} />
+                                <span>70</span>
+                            </button>
+                        </li>
+                        <li>
+                            <a>
+                                2 comments
+                            </a>
+                        </li>
+                    </SocialCounts>
+                    <SocialActions>
+                        <button>
+                            <img src={likes} style={{ width: '28px' }} />
+                            <span>Like</span>
+                        </button>
+                        <button>
+                            <img src={comments} style={{ width: '28px' }} />
+                            <span>Comment</span>
+                        </button>
+                        <button>
+                            <img src={repost} style={{ width: '28px' }} />
+                            <span>Repost</span>
+                        </button>
+                        <button>
+                            <img src={share} style={{ width: '28px' }} />
+                            <span>Send</span>
+                        </button>
+                    </SocialActions>
+                </Article>
+            </div>
         </Container>
     )
 }
@@ -101,6 +166,121 @@ const ShareBox = styled(CommonCard)`
                 span {
                     color: rgba(0, 0, 0, 0.6);
                 }
+            }
+        }
+    }
+`
+const Article = styled(CommonCard)`
+    padding: 0;
+    margin: 0 0 8px;
+    overflow: visible;
+`;
+
+const SharedActor = styled.div`
+    padding-right: 40px;
+    flex-wrap: nowrap;
+    padding: 12px 16px 0;
+    margin-bottom: 8px;
+    align-items: center;
+    display: flex;
+    a {
+        margin-right: 12px;
+        flex-grow: 1;
+        overflow: hidden;
+        display: flex;
+        text-decoration: none;
+
+        img {
+            width: 48px;
+            height: 48px;
+        }
+
+        & > div {
+            display: flex;
+            flex-direction: column;
+            flex-grow: 1;
+            flex-basis: 0;
+            margin-left: 8px;
+            overflow: hidden;
+            span {
+                text-align: left;
+                &:first-child {
+                    font-size: 14px;
+                    font-weight: 700;
+                    color: rgba(0, 0, 0, 1);
+                }
+                &:nth-child(n+1){
+                    font-size: 12px;
+                    color: rgba(0, 0, 0, 0.6);
+                }
+            }
+        }
+    }
+
+    button {
+        position: absolute;
+        right: 12px;
+        top: 0;
+        background: transparent;
+        border: none;
+        outline: none;
+    }
+`
+
+const Description = styled.div`
+    padding: 0 16px;
+    overflow: hidden;
+    color: rgba(0, 0, 0, 0.9);
+    font-size: 14px;
+    text-align: left;
+`
+
+const SharedImg = styled.div`
+    margin-top: 8px;
+    width: 100%;
+    display: block;
+    position: relative;
+    background-color: #f9fafb;
+    img{
+        object-fit: contain;
+        width: 100%;
+        height: 100%;
+    }
+`
+
+const SocialCounts = styled.ul`
+    line-height: 1.3;
+    display: flex;
+    align-items: flex-start;
+    overflow: auto;
+    margin: 0 16px;
+    padding: 8px 0;
+    border-bottom: 1px solid #e9e5df;
+    list-style: none;
+    li {
+        margin-right: 5px;
+        font-size: 12px;
+        button {
+            display: flex;
+        }
+    }
+`
+
+const SocialActions = styled.div`
+    align-items: center;
+    display: flex;
+    margin: 0;
+    min-height: 40px;
+    padding: 4px 8px;
+    button {
+        display: inline-flex;
+        align-items: center;
+        padding: 8px;
+        color: rgba(0, 0, 0, 0.6);
+
+        @media(min-width: 768px) {
+            span {
+                margin-left: 8px;
             }
         }
     }
